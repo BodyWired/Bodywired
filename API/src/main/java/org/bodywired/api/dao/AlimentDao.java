23724c4e2fea89b00f8167e26dea5588a611b69f
Garcia Jerome <garcia@b10p20.fil.univ-lille1.fr>
2014-10-22 10:57:11 +0200

    MAJ Controller/Service/DAO sur les aliments + création d'un wrapper.

--- API/src/main/java/org/bodywired/api/dao/AlimentDao.java
+++ API/src/main/java/org/bodywired/api/dao/AlimentDao.java
@@ -1,13 +1,24 @@
 package org.bodywired.api.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 import org.bodywired.api.model.Aliment;
 import org.bodywired.api.model.Declinaison;
+import org.bodywired.api.model.classement.CategorieAliment;
+import org.bodywired.api.wrapper.RechercheWrapper;
 
 public interface AlimentDao {
-	
-	public Integer sauvegarderAliment(@Param("ali") Aliment aliment);
-	
-	public Integer sauvegarderDeclinaison(@Param("dec") Declinaison declinaison);
+
+	public Integer sauvegarderAliment ( @Param ( "ali" ) Aliment aliment );
+
+	public Integer sauvegarderDeclinaison ( @Param ( "dec" ) Declinaison declinaison );
+
+	// TODO
+	public List <CategorieAliment> getCategoriesAliments ();
+
+	public List <Aliment> getAliments ( RechercheWrapper wrapper );
+
+	public int getTotal ();
 
 }
---
+++ API/src/main/java/org/bodywired/api/wrapper/RechercheWrapper.java
@@ -0,0 +1,32 @@
+package org.bodywired.api.wrapper;
+
+public class RechercheWrapper {
+
+	private String	filtre;
+	private int		offset;
+	private int		limit;
+
+	public int getLimit () {
+		return limit;
+	}
+
+	public void setLimit ( int limit ) {
+		this.limit = limit;
+	}
+
+	public int getOffset () {
+		return offset;
+	}
+
+	public void setOffset ( int offset ) {
+		this.offset = offset;
+	}
+
+	public String getFiltre () {
+		return filtre;
+	}
+
+	public void setFiltre ( String filtre ) {
+		this.filtre = filtre;
+	}
+}
