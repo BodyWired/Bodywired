daff37d2fa5f1e33158c2c5001b825fbd23b238b
Garcia Jerome <garcia@b10p20.fil.univ-lille1.fr>
2014-10-22 10:46:13 +0200

    Update AlimentService.java

--- API/src/main/java/org/bodywired/api/service/AlimentService.java
+++ API/src/main/java/org/bodywired/api/service/AlimentService.java
@@ -5,17 +5,20 @@ import java.util.List;
 import org.bodywired.api.model.Aliment;
 import org.bodywired.api.model.Declinaison;
 import org.bodywired.api.model.classement.CategorieAliment;
+import org.bodywired.api.wrapper.RechercheWrapper;
 
 public interface AlimentService {
 
-	public boolean ajouterAlimentDansCategorie(
-			CategorieAliment categorieAliment, Aliment aliment);
+	public boolean ajouterAlimentDansCategorie ( CategorieAliment categorieAliment, Aliment aliment );
 
-	public boolean ajouterDeclinaisonAliment(Aliment aliment,
-			Declinaison declinaison);
+	public boolean ajouterDeclinaisonAliment ( Aliment aliment, Declinaison declinaison );
 
-	public void sauvegarderAlmient(Aliment aliment);
+	public void sauvegarderAliment ( Aliment aliment );
 
-	public List<CategorieAliment> getCategoriesAliements();
+	public List <CategorieAliment> getCategoriesAliments ();
+
+	public List <Aliment> getAliments ( RechercheWrapper wrapper );
+
+	public int getTotal ();
 
 }
